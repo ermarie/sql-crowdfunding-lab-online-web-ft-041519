3291bf5883ab2dad3ac82000ef10ab6ed2683177
Erin Stalzer <erinstalzer@gmail.com>
2019-05-22 16:47:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,8 +10,8 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT projects.title, pledges.amount FROM projects
 INNER JOIN pledges
 ON pledges.project_id = projects.id
-GROUP BY projects.id
 SUM(pledges.amount)
+GROUP BY projects.id
 HAVING pledges.project_id = projects.id
 ORDER BY projects.title ASC;"
 end
