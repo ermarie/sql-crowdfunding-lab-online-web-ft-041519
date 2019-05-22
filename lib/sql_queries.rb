a7ac49d1ffcd7662fd3398433a8dab6b56329413
Erin Stalzer <erinstalzer@gmail.com>
2019-05-22 16:58:30 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ ORDER BY users.name ASC"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, (sum(pledges.amount) - users.funding.goal) FROM projects
+"SELECT projects.title, (sum(pledges.amount) - users.funding_goal) FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
 GROUP BY projects.oid
