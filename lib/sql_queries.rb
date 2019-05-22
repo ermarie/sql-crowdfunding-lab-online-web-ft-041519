4df2725cd46e2901941da0e953919ae1bb1c3302
Erin Stalzer <erinstalzer@gmail.com>
2019-05-22 16:10:08 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,9 +10,10 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT projects.title, pledges.amount FROM (projects
 INNER JOIN pledges
 ON pledges.project_id = projects.id)
-ORDER BY projects.title ASC
-GROUP BY pledges.amount
-HAVING ;"
+GROUP BY projects.id
+SUM(pledges.amount)
+HAVING pledges.project_id = projects.id
+ORDER BY projects.title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
